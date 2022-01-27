74d2a5b3c88bdb30197274a13da277f8828035f2
rev <69842744+rev4324@users.noreply.github.com>
2022-01-27 17:28:05 +0100

    theme state logic

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,14 +1,17 @@
+import { useState } from 'react';
 import '../styles/globals.css'
 import Header from '../components/Header'
 import Settings from '../components/Settings'
 
 function App({ Component, pageProps }) {
+  const [theme, setTheme] = useState("light")
   return (
     <>
       <Header />
-      <Component {...pageProps} />
-      <Settings />
+        <Component {...pageProps } />
+      <Settings theme={theme} setTheme={setTheme} />
     </>
+
   )
 }
 
