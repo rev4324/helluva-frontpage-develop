d62dff66a75c960385fb626c39a7f80146783035
rev <69842744+rev4324@users.noreply.github.com>
2022-04-06 22:03:19 +0200

    fix React hydration error

--- next.config.js
+++ next.config.js
@@ -1,13 +1,23 @@
-module.exports = {
-  reactStrictMode: true,
-}
-
 const withReactSvg = require('next-react-svg')
 const path = require('path')
 
-module.exports = withReactSvg({
+const nextconfig = withReactSvg({
+  reactStrictMode: true,
+  compiler: {
+    styledComponents: true,
+  },
   include: path.resolve(__dirname, 'public/svg'),
   webpack(config, options) {
     return config
-  }
-})
+  },
+});
+
+
+// const svgconfig = withReactSvg({
+//   include: path.resolve(__dirname, 'public/svg'),
+//   webpack(config, options) {
+//     return config
+//   }
+// })
+
+module.exports = nextconfig;
