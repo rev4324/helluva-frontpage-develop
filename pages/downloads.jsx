59b8ce7511c8b2cc6cad4389e65d91b349fa1747
rev <69842744+rev4324@users.noreply.github.com>
2022-04-06 22:03:38 +0200

    add content

--- pages/downloads.jsx
+++ pages/downloads.jsx
@@ -1,7 +1,26 @@
+import styled from 'styled-components'
+import MainBox from '../components/MainBox'
+import Box from '../components/Box'
+
+const MainBoxCustom = styled(MainBox)`
+    justify-content: center;
+`
+
+const BoxCustom = styled(Box)`
+    justify-content: center;
+`
+
+const SoonTM = styled.p`
+    font-size: 3em;
+    font-weight: 800;
+`
+
 export default function Downloads() {
     return (
-        <div>
-            <p>Yes</p>
-        </div>
+        <MainBoxCustom style={{height: "1000px"}}>
+            <BoxCustom>
+                <SoonTM>Coming soon.</SoonTM>
+            </BoxCustom>
+        </MainBoxCustom>
     )
 }
